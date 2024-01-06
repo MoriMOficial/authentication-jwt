2ce773a64f6548d117e4ff3ee1278991813e7f17
MoriM Oficial <morimworkoficial@gmail.com>
2024-01-06 07:21:13 -0300

    router express working

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-
+const router = require('../routes/router.js')
 const app = express();
+
+app.use(router)
 
 
 module.exports = app
