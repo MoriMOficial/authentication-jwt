650e2993bde15861742f428f0a203e556435caf0
Gabriel Henrique das Neves <136454707+MoriMOficial@users.noreply.github.com>
2024-01-09 10:24:41 -0300

    Update server.js
    Removing "const logged = " from "conn()"

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 const PORT = process.env.PORT;
 
-const logged = conn((value) => {
+conn((value) => {
     if(value != true)  {
         console.log("Something goes wrong with the database".red)
         return;
